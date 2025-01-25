18fc685eed8e0fdf41d1db683c9ad11720146f82
ji young park <moa00_@naver.com>
2025-01-26 05:45:44 +0900

    체력바 복구

--- Cyphers/Source/Cyphers/UI/CyphersHUDWidget.cpp
+++ Cyphers/Source/Cyphers/UI/CyphersHUDWidget.cpp
@@ -13,24 +13,24 @@ UCyphersHUDWidget::UCyphersHUDWidget(const FObjectInitializer& ObjectInitializer
 void UCyphersHUDWidget::UpdateStat(const FCyphersCharacterStat& BaseStat, const FCyphersCharacterStat& ModifierStat)
 {
 	FCyphersCharacterStat TotalStat = BaseStat + ModifierStat;
-	//HpBar->UpdateStat(BaseStat, ModifierStat);
-	//CharacterStat->UpdateStat(BaseStat, ModifierStat);
+	HpBar->UpdateStat(BaseStat, ModifierStat);
+	CharacterStat->UpdateStat(BaseStat, ModifierStat);
 }
 
 void UCyphersHUDWidget::UpdateHpBar(float NewCurrentHp)
 {
-	//HpBar->UpdateHpBar(NewCurrentHp);
+	HpBar->UpdateHpBar(NewCurrentHp);
 }
 
 void UCyphersHUDWidget::NativeConstruct()
 {
 	Super::NativeConstruct();
 
-	//UUserWidget* test = Cast<UUserWidget>(GetWidgetFromName(TEXT("WBP_HpBar")));
-	//ensure(test);
+	HpBar = Cast<UCyphersHpBarWidget>(GetWidgetFromName(TEXT("WBP_HpBar")));
+	ensure(HpBar);
 
-	//CharacterStat = Cast<UCyphersCharacterStatWidget>(GetWidgetFromName(TEXT("WBP_CharacterStat")));
-	//ensure(CharacterStat);
+	CharacterStat = Cast<UCyphersCharacterStatWidget>(GetWidgetFromName(TEXT("WBP_CharacterStat")));
+	ensure(CharacterStat);
 
 	ICyphersCharacterHUDInterface* HUDPawn = Cast<ICyphersCharacterHUDInterface>(GetOwningPlayerPawn());
 	if (HUDPawn)
